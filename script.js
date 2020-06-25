b6a23b238da6f6509c2a5a45fc0777a6fd60f571
Courtney Paasch <40651335+courtneypaasch@users.noreply.github.com>
2020-06-25 00:17:31 -0400

    add check for at least one selection

--- script.js
+++ script.js
@@ -14,46 +14,59 @@ var includeSpecialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/",
 var includeNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var includeLowLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var includeCapLetters = includeLowLetters.map(v => v.toUpperCase());
-var  newArray = null;
+var newArray = null;
 var passwordBuilder;
 
 //Gather user input for variable data
 function gatherInput() {
   newPassword.length = prompt("Set password length between 8 and 128 character:");
+  
   while (newPassword.length < 8 || newPassword.length > 128) {
     newPassword.length = prompt("Length must be between 8 and 128 character. How long would you like?");
   }
+
   newPassword.specialChar = confirm("Would you like to include special characters?");
   newPassword.numbers = confirm("Would you like to include numbers?");
   newPassword.capLetters = confirm("Would you like to include capital letters?");
   newPassword.lowLetters = confirm("Would you like to include lowercase letters?");
+
+  while (newPassword.specialChar === false && newPassword.numbers === false && newPassword.capLetters === false && newPassword.lowLetters === false) {
+    alert("At least one character option must be selected. Your password will be " + newPassword.length + " characters long. Try again.");
+    newPassword.specialChar = confirm("Would you like to include special characters?");
+    newPassword.numbers = confirm("Would you like to include numbers?");
+    newPassword.capLetters = confirm("Would you like to include capital letters?");
+    newPassword.lowLetters = confirm("Would you like to include lowercase letters?");
+
+  }
+
+
 }
 //Determines which characters to include in the random selection based on user preferences
 function dataType() {
   newArray = null;
 
   if (newPassword.specialChar === true) {
-     newArray = includeSpecialChar;
-    }
+    newArray = includeSpecialChar;
+  }
   if (newPassword.numbers === true && newArray === null) {
     newArray = includeNumbers;
-   }
- else if (newPassword.numbers === true && newArray !== null) {
-   newArray = includeNumbers.concat(newArray);
- }
- if (newPassword.lowLetters === true && newArray === null) {
-  newArray = includeLowLetters;
- }
-else if (newPassword.lowLetters === true && newArray !== null) {
- newArray = includeLowLetters.concat(newArray);
-}
-if (newPassword.capLetters === true && newArray === null) {
-  newArray = includeCapLetters;
- }
-else if (newPassword.capLetters === true && newArray !== null) {
- newArray = includeCapLetters.concat(newArray);
-}
-return newArray;
+  }
+  else if (newPassword.numbers === true && newArray !== null) {
+    newArray = includeNumbers.concat(newArray);
+  }
+  if (newPassword.lowLetters === true && newArray === null) {
+    newArray = includeLowLetters;
+  }
+  else if (newPassword.lowLetters === true && newArray !== null) {
+    newArray = includeLowLetters.concat(newArray);
+  }
+  if (newPassword.capLetters === true && newArray === null) {
+    newArray = includeCapLetters;
+  }
+  else if (newPassword.capLetters === true && newArray !== null) {
+    newArray = includeCapLetters.concat(newArray);
+  }
+  return newArray;
 }
 
 
@@ -65,19 +78,16 @@ function randomSelector() {
     var passwordCharacter = newArray[Math.floor(Math.random() * newArray.length)];
     passwordBuilder = passwordBuilder + passwordCharacter;
   }
-  return (passwordBuilder); 
+  return (passwordBuilder);
 }
 
-  
+
 //Function called in order to generate password
 function generatePassword() {
   gatherInput();
   dataType();
   randomSelector();
-  console.log(newArray); 
   return passwordBuilder;
-  
-
 }
 
 
